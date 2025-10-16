bda7cfe29948eeec4847898f1139d339222f0902
MiguelGonz06 <miguel.gonzalezsa@elorrieta-errekamari.com>
2025-10-16 10:29:19 +0200

    Initial Commit Sprint 1

---
+++ TitanFit/src/Vista/Main.java
@@ -0,0 +1,10 @@
+package Vista;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Login frame = new Login();
+		frame.setVisible(true);
+	}
+
+}
--- TitanFit/src/module-info.java
+++ TitanFit/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module TitanFit {
+	requires java.desktop;
 }
